ea325a42d974ad79363c836695f76c3bb3e3ac37
Marcos Camacho <marcoscachis@gmail.com>
2023-01-30 12:57:50 -0600

    change  local db to mongodb cluster

--- api/v1/api_v1.routes.js
+++ api/v1/api_v1.routes.js
@@ -20,9 +20,9 @@ router.post("/register", async (req, res) => {
     await user.save();
 
     res.status(201).json({
-		message:"User created",
-		user
-	});
+		  message:"User created",
+		  user
+	  });
   } catch (error) {
     console.log(error.message);
   }
--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/jwt_app", {
+        await mongoose.connect(`mongodb+srv://cachiss:${process.env.MONGO_PASSWORD}@cluster0.rvmyh73.mongodb.net/jwt_app`, {
             useNewUrlParser: true,
         });
         console.log('MongoDB connected');
