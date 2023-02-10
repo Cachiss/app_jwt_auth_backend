44b68beaff018a5bfb13044ce8ffcdff0b41fd73
Marcos Camacho <marcoscachis@gmail.com>
2023-02-09 22:39:59 -0600

    add connection to app

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 
 const routes = require('./routes/index.routes');
 const apiV1Routes = require('./api/v1/api_v1.routes');
-
+const connectDb = require('./db/connection');
 // Config dotenv & port
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
 // Options
+connectDb();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
